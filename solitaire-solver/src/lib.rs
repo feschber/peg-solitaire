4d877c055f3e3c66c295ce75c1c5e0af18d81f92
Ferdinand Schober <ferdinand.schober@fau.de>
2025-08-25 02:44:13 +0200

    add back the naive solution for comparison

--- solitaire-solver/src/lib.rs
+++ solitaire-solver/src/lib.rs
@@ -151,6 +151,56 @@ pub fn calculate_all_solutions() -> Vec<Board> {
     solvable
 }
 
+pub fn calculate_all_solutions_naive() -> Vec<Board> {
+    fn solve_all(
+        board: Board,
+        already_checked: &mut HashSet<Board>,
+        solvable: &mut HashSet<Board>,
+    ) -> bool {
+        // board is solved
+        if board.is_solved() {
+            solvable.insert(board);
+            already_checked.insert(board);
+            return true;
+        }
+
+        // found a known configuration
+        if already_checked.contains(&board) {
+            return solvable.contains(&board);
+        }
+
+        let mut any_solution = false;
+        for y in 0..Board::SIZE {
+            for x in 0..Board::SIZE {
+                if board.occupied((y, x)) {
+                    for dir in [Dir::North, Dir::East, Dir::South, Dir::West] {
+                        if let Some(mov) = board.get_legal_move((y, x), dir) {
+                            any_solution |=
+                                solve_all(board.mov(mov).normalize(), already_checked, solvable);
+                        }
+                    }
+                }
+            }
+        }
+        already_checked.insert(board);
+        if any_solution {
+            solvable.insert(board);
+        }
+        any_solution
+    }
+    let mut solvable = HashSet::new();
+    let mut already_checked = HashSet::new();
+    solve_all(Board::default(), &mut already_checked, &mut solvable);
+    let total = already_checked.len();
+    let solvable_count = solvable.len();
+    assert_eq!(solvable_count, 1679073);
+    println!(
+        "checked {total} constellations, {solvable_count} have a solution ({:.2}%)",
+        (solvable_count as f64 / total as f64) * 100.
+    );
+    solvable.into_iter().collect()
+}
+
 pub fn print_solution(solution: Solution) {
     let mut board = Board::default();
     println!("{board}");
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 fn main() {
     if std::env::args().any(|a| &a == "-a") {
         solitaire_solver::calculate_all_solutions();
+    } else if std::env::args().any(|a| &a == "-n") {
+        solitaire_solver::calculate_all_solutions_naive();
     } else if std::env::args().any(|a| &a == "-s") {
         let solution = solitaire_solver::calculate_first_solution();
         solitaire_solver::print_solution(solution);
